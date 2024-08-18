b457aee4d743e51c3186e2a0a13779e314412882
Abhay <abhay523@gmail.com>
2024-08-18 13:03:59 +0530

    Update FileUtilities.js

--- FileUtilities.js
+++ FileUtilities.js
@@ -1,10 +1,31 @@
 /**
- * Returns the extension from a file object by querying its MIME type.
- * @param {GoogleAppsScript.Drive.File} file - The file from which to extract the extension.
- * @return {string} The file extension.
+ * Returns a folder name for a given file object based on its MIME type, using a predefined mapping. If the MIME type
+ * is not found in the predefined mapping, it appends the MIME type to a Google Spreadsheet for review and derives the folder name from the MIME type itself.
+ * @param {GoogleAppsScript.Drive.File} file - The file for which to determine the folder name.
+ * @return {string} The folder name associated with the file's MIME type or derived from the MIME type.
  */
-const getExtension = (file) => {
+const getFolderNameForFile = (file) => {
   const mime = file.getMimeType();
-  const docType = mime.split("/").pop();
-  return docType.split(".").pop();
+
+  // Attempt to find the MIME type in the predefined mapping.
+  if (mimeTypeToFolderName.hasOwnProperty(mime)) {
+    return capitalizeFirstLetters(mimeTypeToFolderName[mime]);
+  } else {
+    // Derive the folder name from the MIME type's subtype.
+    let derivedFolderName = mime.split('/').pop().split('.').pop(); // Safeguard against MIME types with '.' in the subtype
+    derivedFolderName = derivedFolderName.replace(/_/g, ' '); // Replace underscores with spaces
+    return capitalizeFirstLetters(derivedFolderName);
+  }
 };
+
+
+/**
+ * Capitalizes the first letter of each word in a string.
+ * @param {string} str - The string to capitalize.
+ * @return {string} The capitalized string.
+ */
+function capitalizeFirstLetters(str) {
+  return str.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+
